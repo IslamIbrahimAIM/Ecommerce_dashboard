d1fbac794215969f527537e427861757b6fa6ad1
islam ibrahim <islamibrahim008@gmail.com>
2024-05-25 18:23:20 +0400

    adding Procfile and setup.sh for deployment

---
+++ ecom.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    """ 
+    Testing Streamlit
+    """
+    st.title("Ecom Dashboard")
+    st.subheader('Hello Islam')
+
+
+if __name__ == '__main__':
+    main()
