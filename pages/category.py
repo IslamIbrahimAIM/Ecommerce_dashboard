2ad1b37e260398134ab46465b169293bc01d722a
islam ibrahim <islamibrahim008@gmail.com>
2024-06-17 13:17:54 +0400

    adding coming soon page

--- pages/category.py
+++ pages/category.py
@@ -11,9 +11,16 @@ import math
 
 
 def app():
-    st.title("Categories")
+    st.title("Univariate Analysis")
 
+    st.write("We're working hard to bring you something amazing! :rocket:")
 
+    st.write("Stay tuned for updates.")
+
+    # Replace 'https://example.com/coming_soon_image.png' with your actual image URL from Splash or any other source
+    image_url = 'https://images.unsplash.com/photo-1614332287897-cdc485fa562d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29taW5nJTIwc29vbnxlbnwwfHwwfHx8MA%3D%3D'
+
+    st.image(image_url, use_column_width=True)
 
 
 
