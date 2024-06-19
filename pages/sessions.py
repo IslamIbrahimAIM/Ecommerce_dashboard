ea49f75805a5467b3a3f14d99e27df6bf98e7b43
islam ibrahim <islamibrahim008@gmail.com>
2024-06-19 08:41:04 +0400

    hiding index

--- pages/sessions.py
+++ pages/sessions.py
@@ -611,7 +611,7 @@ def app():
         st.plotly_chart(buckets, use_container_width=True, config=config)
     st.divider()
     filtered_brand = filtered_brand.sort_values(by='Sales', ascending=False)
-    st.dataframe(filtered_brand, hide_index=True, column_order=['brand', 'Views', 'Sales', 'Orders', 'Buyers'], use_container_width=True, hide_index=True)
+    st.dataframe(filtered_brand, hide_index=True, column_order=['brand', 'Views', 'Sales', 'Orders', 'Buyers'], use_container_width=True)
     filter_col1, filter_col2 = st.columns(2)
     with filter_col1:
         custom_order = ["Star", "High Performance", "Low Performance", "Weak Performance"]
